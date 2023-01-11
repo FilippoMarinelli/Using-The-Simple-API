d92dd7d416a63f32707342cd6a39a9878357f535
Filippo Marinelli <lipo-x123@outlook.com>
2023-01-11 15:36:30 -0300

    project base

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,46 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+import { primaryDarkColor, primaryColor } from '../config/colors';
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+
+  body{
+    font-family: sans-serif;
+    background: ${primaryDarkColor};
+  }
+
+  html,body, #root{
+    height: 100%;
+  }
+
+  button{
+    cursor: pointer;
+    background: ${primaryColor};
+    border: none;
+    color: #fff;
+    padding: 10px;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul{
+    list-style: none;
+  }
+`;
+
+export const Container = styled.section`
+  max-width: 900px;
+  background: #fff;
+  margin: 30px auto;
+  padding: 30px;
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgb(0, 0, 0, 0.4);
+`;
